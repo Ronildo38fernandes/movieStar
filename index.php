e0bea75d693c47b9cc762d534002fc2c9c0e5b4f
Ronildo Fernandes <ronildo38fernandes@hotmail.com>
2023-05-16 13:03:57 -0300

    Projeto em andamento

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+  require_once("templates/header.php");
+?>
+  <div id="main-container" class="container-fluid">
+    <h1>Corpo do site</h1>
+  </div>
+
+
+      
+ 
+</body>
+</html>
+<?php
+  require_once("templates/footer.php");
+?>
